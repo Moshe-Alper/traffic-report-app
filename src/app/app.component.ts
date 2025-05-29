3864e17bf5c9c9e35c8b842e6fdfc582ec45d68a
Moshik Alper <moshikalper@gmail.com>
2025-05-29 17:38:05 +0300

    Split cmps into multiple comps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,15 @@
 import { Component } from '@angular/core';
+import { AppHeaderComponent } from "./cmps/app-header/app-header.component";
+import { ServerStatusComponent } from "./cmps/dashboard/server-status/server-status.component";
+import { TrafficComponent } from "./cmps/dashboard/traffic/traffic.component";
+import { TicketsComponent } from "./cmps/dashboard/tickets/tickets.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
+  imports: [AppHeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent],
 })
 export class AppComponent {
-  dummyTrafficData = [
-    {
-      id: 'd1',
-      value: 433,
-    },
-    {
-      id: 'd2',
-      value: 260,
-    },
-    {
-      id: 'd3',
-      value: 290,
-    },
-    {
-      id: 'd4',
-      value: 410,
-    },
-    {
-      id: 'd5',
-      value: 397,
-    },
-    {
-      id: 'd6',
-      value: 488,
-    },
-    {
-      id: 'd47',
-      value: 589,
-    },
-  ];
-  maxTraffic = Math.max(...this.dummyTrafficData.map((data) => data.value));
-  currentStatus = 'online';
+ 
 }
