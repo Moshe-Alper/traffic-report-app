effe50ce6b68be0aded012035c83319ffdea3fe6
Moshik Alper <moshikalper@gmail.com>
2025-05-30 14:42:59 +0300

    Refactor dashboard components to use new DashboardItemComponent and streamline HTML structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { AppHeaderComponent } from "./cmps/app-header/app-header.component";
 import { ServerStatusComponent } from "./cmps/dashboard/server-status/server-status.component";
 import { TrafficComponent } from "./cmps/dashboard/traffic/traffic.component";
 import { TicketsComponent } from "./cmps/dashboard/tickets/tickets.component";
+import { DashboardItemComponent } from "./cmps/dashboard/dashboard-item/dashboard-item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [AppHeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent],
+  imports: [AppHeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent, DashboardItemComponent],
 })
 export class AppComponent {
  
