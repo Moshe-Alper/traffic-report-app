75837bbc425853bbf2aec9947ee16a42474d2441
Moshik Alper <moshikalper@gmail.com>
2025-05-31 16:56:10 +0300

    Enhance new ticket component with form submission handling; add ViewChild for form reference and integrate FormsModule

--- src/app/cmps/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/cmps/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 import { AppButtonComponent } from '../../../../shared/app-button/app-button.component';
 import { ControlComponent } from "../../../../shared/control/control/control.component";
 
 @Component({
   selector: 'new-ticket',
   standalone: true,
-  imports: [AppButtonComponent, ControlComponent],
+  imports: [AppButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>
 
+  onSubmit(title: string, ticketText: string) {
+    console.log(title, ticketText)
+    this.form?.nativeElement.reset()
+  }
 }
