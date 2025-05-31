dc5e14b7a333f527f1567741e606051b7d42d867
Moshik Alper <moshikalper@gmail.com>
2025-05-31 19:19:59 +0300

    Refactor new-ticket and control components to utilize ViewChild and contentChild for improved form and input handling

--- src/app/cmps/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/cmps/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 import { AppButtonComponent } from '../../../../shared/app-button/app-button.component';
 import { ControlComponent } from "../../../../shared/control/control/control.component";
@@ -10,11 +10,21 @@ import { ControlComponent } from "../../../../shared/control/control/control.com
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
-export class NewTicketComponent {
-  @ViewChild('form') form?: ElementRef<HTMLFormElement>
+export class NewTicketComponent implements OnInit, AfterViewInit {
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form')
+
+  ngOnInit() {
+    console.log('On Init')
+    console.log(this.form().nativeElement)
+  }
+
+  ngAfterViewInit(): void {
+    console.log('After View Init')
+    console.log(this.form().nativeElement)
+  }
 
   onSubmit(title: string, ticketText: string) {
     console.log(title, ticketText)
-    this.form?.nativeElement.reset()
+    this.form().nativeElement.reset()
   }
 }
